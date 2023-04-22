a10403400deb41e6e61db27529ecbc9afdacb1fc
Eric Capo <eric.capo@hotmail.fr>
2023-04-22 19:02:02 +0200

    Create sidebar.js

---
+++ js/sidebar.js
@@ -0,0 +1,4 @@
+// Open the sidebar when the hamburger icon is clicked
+document.querySelector('.hamburger').addEventListener('click', function() {
+	document.querySelector('#sidebar').classList.toggle('open');
+});
